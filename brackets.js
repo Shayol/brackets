105654a4b74a79cf692a494e8fb509ab7b0817b0
mila <vorga88@gmail.com>
2018-08-09 14:27:34 +0300

    check three types of brackets

--- brackets.js
+++ brackets.js
@@ -1,30 +1,38 @@
 function brackets(str) {
-    let arr = [...str];
-    let left = arr.reduce((acc,el,i) => {
-        if(el == "(") {
-            acc.push(i);
-        }
-        return acc;
-    }, []);
-    let right = arr.reduce((acc,el,i) => {
-        if(el == ")") {
-            acc.push(i);
-        }
-        return acc
-    }, []);
 
-    if(left.length != right.length) {
+    let result = false;
+
+    result = checkBracket("(",")",str) && checkBracket("[","]",str) && checkBracket("{","}",str);
+    
+    function checkBracket(leftBracket, rightBracket, str) {
+        let arr = [...str];
+        let left = arr.reduce((acc, el, i) => {
+            if (el == leftBracket) {
+                acc.push(i);
+            }
+            return acc;
+        }, []);
+        let right = arr.reduce((acc, el, i) => {
+            if (el == rightBracket) {
+                acc.push(i);
+            }
+            return acc
+        }, []);
+
+        if (left.length != right.length) {
+            return false;
+        }
+
+        let leftSum = left.reduce((acc, el) => acc + el);
+        let rightSum = right.reduce((acc, el) => acc + el);
+
+        if (leftSum + left.length <= rightSum) {
+            return true;
+        }
         return false;
     }
-
-    let leftSum = left.reduce((acc,el) => acc + el);
-    let rightSum = right.reduce((acc,el) => acc + el);
-
-    if(leftSum + left.length <= rightSum) {
-        return true;
-    }
-    return false;
+    return result;
 }
 
-console.log(brackets("(аааа(б))(сссс)"));
-console.log(brackets("(аааа(б)(сссс)"));
+console.log(brackets("[{(аа(б))}](сссс)"));
+console.log(brackets("(аааа{(б)}(сссс)]["));
